638d163fc50cd78b8c7ec8b3b320b5e0b1ec7e4a
EC2 Default User <ec2-user@ip-172-31-12-120.ap-southeast-2.compute.internal>
2014-09-14 13:06:43 +0000

    first commit

---
+++ SimpleWebRTC/build.js
@@ -0,0 +1,17 @@
+var bundle = require('browserify')(),
+    fs = require('fs'),
+    request = require('request'),
+    uglify = require('uglify-js');
+
+bundle.add('./simplewebrtc');
+bundle.bundle({standalone: 'SimpleWebRTC'}, function (err, source) {
+    if (err) console.error(err);
+    fs.writeFileSync('simplewebrtc.bundle.js', source);
+    request.get('http://54.79.14.231:8888/node_modules/socket.io/lib/socket.io.js', function (err, res, body) {
+        if (!err && body && body.length) {
+            fs.writeFile('latest.js', uglify.minify(source + body, {fromString: true}).code, function (err) {
+                if (err) throw err;
+            });
+        }
+    });
+});

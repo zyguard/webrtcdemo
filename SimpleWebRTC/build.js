c2f2879f3f6e43dc5f2e981b7d6c435e459628fd
zyguard <zyguard@gmail.com>
2014-09-17 14:13:29 +0800

    Update build.js

--- SimpleWebRTC/build.js
+++ SimpleWebRTC/build.js
@@ -7,7 +7,7 @@ bundle.add('./simplewebrtc');
 bundle.bundle({standalone: 'SimpleWebRTC'}, function (err, source) {
     if (err) console.error(err);
     fs.writeFileSync('simplewebrtc.bundle.js', source);
-    request.get('http://54.79.14.231:8888/node_modules/socket.io/lib/socket.io.js', function (err, res, body) {
+    request.get('https://54.79.14.231:8857/node_modules/socket.io/lib/socket.io.js', function (err, res, body) {
         if (!err && body && body.length) {
             fs.writeFile('latest.js', uglify.minify(source + body, {fromString: true}).code, function (err) {
                 if (err) throw err;
